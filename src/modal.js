3b7039e7a4d7534ef5b8fdb994cdac604590b21e
Oleksandr S <oleksandr.slv@gmail.com>
2021-09-13 20:21:27 +0300

    close modal on overlay, adds mobile modal btn

--- src/modal.js
+++ src/modal.js
@@ -1,12 +1,17 @@
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open]'),
+    openMobileModalBtn: document.querySelector('[data-modal-mobile-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openMobileModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.modal.addEventListener('click', (e) => {
+    if (e.currentTarget === e.target) toggleModal();
+  })
 
   function toggleModal() {
     document.body.classList.toggle('modal-open');
