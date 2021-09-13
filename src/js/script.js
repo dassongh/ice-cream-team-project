875184c45e888a681339fa6b4936d66174fe4cf6
Askarunro <paskar.y.a@gmail.com>
2021-09-14 02:25:49 +0300

    js

---
+++ src/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.container-slider').slick({
+        arrows: false,
+        dots: true,
+        speed: 200,
+        easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
+        infinite: false,
+        });
+});
+// $(document).jquery(function(){
+//     $('.container-slider').slick({
+//                 arrows: false,
+//                 dots: true,
+//                 speed: 200,
+//                 easing: 'cubic-bezier(0.4, 0, 0.2, 1)',
+//                 infinite: false,
+//                 });
+// });
