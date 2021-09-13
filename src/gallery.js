8c0006c7bef50da7b53ced11d145ddbe076ce974
Oleksandr S <oleksandr.slv@gmail.com>
2021-09-13 16:22:23 +0300

    adds desktop styles

--- src/gallery.js
+++ src/gallery.js
@@ -1,11 +1,10 @@
 import Masonry from "masonry-layout";
 
 window.onload = () => {
-  const galleryRef = document.querySelector('.gallery');
+  const galleryRef = document.querySelector('.gallery__items');
 
   const masonry = new Masonry(galleryRef, {
     itemSelector: '.gallery__item',
-    gutter: 7,
-    
+    gutter: 10,
   });
 };
