243e7fcad51a5384649eef4440c09d410b02e0cb
Oleksandr S <oleksandr.slv@gmail.com>
2021-09-13 20:31:06 +0300

    adds close on overlay

--- src/modal-about.js
+++ src/modal-about.js
@@ -7,6 +7,9 @@
 
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.modal.addEventListener('click', (e) => {
+    if (e.currentTarget === e.target) toggleModal();
+  })
 
   function toggleModal() {
     refs.modal.classList.toggle("backdrop-about__is-hidden");
